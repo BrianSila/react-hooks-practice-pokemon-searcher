86e459a01f9ddbfbf1d087eb99673a4921a80d99
Brian <brian.sila1@student.moringaschool.com>
2025-05-03 11:36:54 +0300

    Display pokemon data

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,25 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Card } from "semantic-ui-react";
 
 function PokemonCard() {
-  return (
-    <Card>
+  const [Pokemon, SetPokemon] = useState([]);
+  const [flipimg, Setflipimg] = useState({});
+  
+  function handleimgflip(id) {
+    Setflipimg((prevState) => ({
+      ...prevState,
+      [id]: !prevState[id],
+    }));
+  }
+
+  useEffect(() => {
+    fetch("http://localhost:3001/pokemon")
+      .then((response) => response.json())
+      .then((data) => SetPokemon(data))
+      .catch((error) => {
+        console.log(`error fetching`, error);
+      });
+  }, []);
+
+  return Pokemon.map((pokemon) => (
+    <Card key={pokemon.id}>
       <div>
-        <div className="image">
-          <img alt="oh no!" />
+        <div className="image" onClick={() => handleimgflip(pokemon.id)}>
+          <img
+            src={
+              flipimg[pokemon.id] ? pokemon.sprites.back : pokemon.sprites.front
+            }
+            alt={`${pokemon.name} sprite`}
+          />
         </div>
         <div className="content">
-          <div className="header">POKEMON NAME HERE</div>
+          <div className="header">{pokemon.name}</div>
         </div>
         <div className="extra content">
           <span>
             <i className="icon heartbeat red" />
-            POKEMON HP HERE hp
+            {pokemon.hp}
           </span>
         </div>
       </div>
     </Card>
-  );
+  ));
 }
 
 export default PokemonCard;
